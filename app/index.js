5e600da721f67df46117842b987b676de4b23113
Louis Christopher <louis9171@outlook.com>
2016-03-20 17:48:26 +0530

    add motivations package

--- app/index.js
+++ app/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
+var motivations = require('motivations');
 var app = express();
+
+app.get('/test', function(request, response){
+	response.send(motivations);		
+});
 app.get('/', function(request, response){
 	response.send("Cha Ching");
 });
